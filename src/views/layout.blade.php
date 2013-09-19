88b4d1d182311d090dcb81698ada63adbee614c3
Matthias Kluth <matthias@kluth.me>
2013-09-19 13:17:09 +0200

    Basic bootstrap functionality.

---
+++ src/views/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adminoberfläche</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Cafe Fleischlos</a>
+		</div>
+	</nav>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="//code.jquery.com/jquery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="dist/js/bootstrap.min.js"></script>
+</body>
+</html>
