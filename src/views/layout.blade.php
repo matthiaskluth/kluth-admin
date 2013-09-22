5a404f5b3b911ed4d0132fcee98e45cad7300f34
Matthias Kluth <matthias@kluth.me>
2013-09-22 16:58:54 +0200

    Added a hello, world.

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -30,6 +30,9 @@
 		    </ul>
 	    </div>
 	</nav>
+	<div class="container">
+		Hello, world!
+	</div>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="//code.jquery.com/jquery.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
